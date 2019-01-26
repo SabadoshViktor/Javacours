b77b2435135e0534dbd5cc6ad0d2add0e513bcd4
Sabadosh Viktor <sabadosh.viktor95@gmail.com.com>
2019-01-26 13:51:25 +0100

    add

---
+++ 2019_01_26_RemoveDuplicatesVithSet/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		List<String> duplicates = Arrays.asList("Ivan", "petr", "Ivan", " Anna");
+		System.out.println(removeDuplicates(duplicates));
+	}
+
+	public static List<String> removeDuplicates(List<String> strings){
+		
+		Set<String> set = new HashSet<>();
+		set.addAll(strings);
+		
+		return new ArrayList<>(set);
+	}
+//	public static List<String> removeDuplicateswithoutSets(List<String> strings){
+//		
+//		Set<String> set = new HashSet<>();
+//		set.addAll(strings);
+//		
+//		return new ArrayList<>(set);
+//	}
+
+}
+
+
+//дз: розібратися без сетів
+//1.List<String> getDuplicates(List<String> strings){}
+//2remove duplicates without Sets
+//розібратися з МАПАМИ (HashMap)

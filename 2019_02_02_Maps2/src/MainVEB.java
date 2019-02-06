e7fe0883074853c120341eed3516ef05798e84da
Your Name <you@example.com>
2019-02-06 17:56:18 +0100

    kol

--- 2019_02_02_Maps2/src/MainVEB.java
+++ 2019_02_02_Maps2/src/MainVEB.java
@@ -23,13 +23,16 @@ public class MainVEB {
 	System.out.println(listA);
 	System.out.println(listB);
 	
+	List<String> strings = new ArrayList<>(Arrays.asList("Java" , "Method", "Jazz", "Rocky", "Lenovo", " Lexus", "Mazda"));
+
+	
 	System.out.println(findDuplicates(listA, listB));
 	
 	}
 	
 	public static List<String> findDuplicates(List<String> listA , List<String> listB){
 		
-		List<String> res = new LinkedList<>();
+		List<String> res = new ArrayList<>();
 		HashSet<String> set = new HashSet<>(listA);
 		  for (String s : listB) {
 		    if (set.contains(s)) {
@@ -38,4 +41,15 @@ public class MainVEB {
 		}
 		return res;
 	}		
+	
+	public static Map<String, List<String>> task(List<String> strings){
+		if((strings==null)||(strings.size()==0)) return Collections.emptyMap();
+		
+		Map<String, List<String>> res = new HashMap<String, List<String>>();
+		
+		
+		
+		return null;
+	}
+	
 }

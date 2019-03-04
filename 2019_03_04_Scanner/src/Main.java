1f1e944063967d2987872d600f224ad937e57a7f
Your Name <you@example.com>
2019-03-04 22:06:13 +0100

    console game

---
+++ 2019_03_04_Scanner/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		consoleGame(3);
+
+	}
+	
+	public static void consoleGame(int quantityTry) {
+		Scanner scanner = new Scanner(System.in);
+		int myNum =22;
+		int count = 0;
+		int number;
+		do {
+			count++;
+			System.out.println("Enter your number: ");
+			number = scanner.nextInt();
+			if(number == myNum) {
+				System.out.println("You win");
+				break;
+			}
+			if(number<myNum && (quantityTry-count)!=0) {
+				System.out.println("No, your number is smaller, try again. You have "+(quantityTry-count)+ " attempts left");
+			}
+			if(number>myNum && (quantityTry-count)!=0) {
+				System.out.println("No, your number is bigger, try again. You have "+(quantityTry-count)+ " attempts left");
+			}
+			if (number!=myNum && (quantityTry-count)==0) {
+				System.out.println("You lose because your attempts is ended");
+			}
+			if(count==quantityTry) {
+				break;
+			}
+		} 
+		while (number!=myNum);
+		}
+	
+	}
